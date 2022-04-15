c994db4ac3fdeed2a6cf10135c519b615f32a964
getzhi <89776560+getzhi@users.noreply.github.com>
2022-04-15 23:48:34 +0800

    Update GitMerge.java

--- git-merge/src/demo/GitMerge.java
+++ git-merge/src/demo/GitMerge.java
@@ -5,6 +5,7 @@ public class GitMerge {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello word github");
+        System.out.println("hello word");
     }
 
 }
